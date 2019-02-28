2fd86018681f75dc1b7fb535b7e222c00e1d2ee1
Fareed Tayar <Fareed Tayar@192.168.2.22>
2019-02-28 17:07:51 -0500

    new sketched gameMainDriver class, with minor change to GameMap class constructor

---
+++ Risk/src/ca/riskgamet31/controllers/GameMainDriver.java
@@ -0,0 +1,36 @@
+package ca.riskgamet31.controllers;
+
+import java.util.ArrayList;
+
+import ca.riskgamet31.maincomps.GameMap;
+import ca.riskgamet31.maincomps.Player;
+
+public class GameMainDriver
+  {
+	
+	private ArrayList<Player> playersList; 
+	private GameMap mainGameMap ;
+		
+	public GameMainDriver()
+	{
+	  playersList = new ArrayList<>();
+	}
+		
+	public static void main(String[] args)
+	  {
+		
+		// call startup phase which has
+			
+			// call initial phase
+			// call builder
+			// call initial distribution of armies
+		
+		// call game play in turn for each player
+			
+					//call player's reinforcement method
+					// call player's attack method
+					// call player's fortification method
+		
+	  }
+	  
+  }
