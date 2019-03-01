cd384d76e179428f45e88c3aeb954046267509c3
Fareed Tayar <Fareed Tayar@172.30.68.211>
2019-03-01 02:52:11 -0500

    added validation methods to ValidationMapInput class.

--- Risk/src/ca/riskgamet31/mapdata/ValidationMapInput.java
+++ Risk/src/ca/riskgamet31/mapdata/ValidationMapInput.java
@@ -1,5 +1,12 @@
 package ca.riskgamet31.mapdata;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import ca.riskgamet31.maincomps.Continent;
+
 /**
  * To validate the map input data.
  * yet to be implemented
@@ -9,21 +16,108 @@ package ca.riskgamet31.mapdata;
  * @since 1.0
  */
 
-
 public class ValidationMapInput 
 {
+  
+  		private Pattern validCountryNamePattern ;
+  		private Matcher matcher ;
+	
+  		public ValidationMapInput()
+  		{
+  		  
+  		validCountryNamePattern = Pattern.compile("^[a-zA-Z0-9]*$");
+  		  
+  		}
+  
+    	/**
+    	 * 	method to validate country names
+    	 * 
+    	 * @param countryName country name to check
+    	 * @return true if valid country name
+    	 */
        public boolean validateCountryName(String countryName)
        {
-    	   return false;
+
+    	 Matcher countryNameMatcher	= validCountryNamePattern.matcher(countryName);
+
+    	   return countryNameMatcher.matches();
        }
        
-       public boolean validateExistingCountry(String countryName)
+       /**
+        * check if country exists
+        * 
+        * @param countryName country name to check
+        * @param countryset existing list of countries 
+        * @return true if country already exist
+        */
+       
+       public boolean checkExistingCountry(String countryName , HashSet<String> countryset)
        {
-    	   return false;
+    	  
+    	 boolean exists = false;
+    	 
+    	 return countryset.stream().anyMatch((x) -> x.equals(countryName)); 
+    	 
        }
        
-       public boolean validateSameCountry(String countryName1,String countryName2)
+       /**
+        * check if both names are equal
+        * 
+        * @param countryName1
+        * @param countryName2
+        * @return true if both name are equal
+        */
+       public boolean checkSimilarNames(String countryName1,String countryName2)
        {
-    	   return false;
+    	   
+    	 return countryName1.equals(countryName2)? true : false; 
        }
+       
+       /**
+        * check if any continent does not have countries associated
+        * @param mapData continents objects
+        * @return true if any continent does not have a country associate with it.
+        */
+       public boolean checkCountinentWithoutCountry(HashMap<String,Continent> mapData)
+       
+       {
+    	 boolean invalid = false;
+    	 
+    	 for (Continent continent : mapData.values())
+    	   {
+    		 
+    		 if (continent.getContinentGraph().getGraphNodes().size() == 0) 
+    		   invalid = true;
+    		 
+    	   }
+    	 
+    	 return invalid;
+    	 
+       }
+       
+       public boolean checkCountryAgainstContinents(String countryName, HashSet<String> continentSet)
+       {
+    	
+    	 return continentSet.contains(countryName);
+    	 
+       }
+       
+       public boolean checkContinentAgainstCountries(String continentName, HashSet<String> countrySet)
+         {
+      	
+      	 return countrySet.contains(continentName);
+      	 
+         }
+       
+       
+       public boolean checkLinks(String fromName, String ToName, HashSet<String> continentSet)
+       
+       {
+    	 
+    	 
+    	 return continentSet.contains(ToName) || continentSet.contains(fromName)|| ToName.equals(fromName);
+    	 
+       }
+       
+      
 }
