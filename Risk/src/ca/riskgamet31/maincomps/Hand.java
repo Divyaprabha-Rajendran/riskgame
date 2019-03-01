d7a578b2ee50287da0b39d7d266767bccb819f65
yash2696 <y_doshi@encs.concordia.ca>
2019-03-01 06:21:22 -0500

    added java docs for Hand class

--- Risk/src/ca/riskgamet31/maincomps/Hand.java
+++ Risk/src/ca/riskgamet31/maincomps/Hand.java
@@ -2,16 +2,37 @@ package ca.riskgamet31.maincomps;
 
 import java.util.ArrayList;
 
+/**
+ * Hand class which contains cards that player has
+ * 
+ * @author YD
+ * @version 1.0
+ * @since 1.0
+ */
 public class Hand {
 	
+	/**
+	 * whether player must turn in cards or not
+	 */
 	private boolean mustTurnInCards;
+	
+	/**
+	 * ArrayList of cards which will be stored by player
+	 */
 	private ArrayList<Card> hand;
 	
+	/**
+	 * Constructor to create Hand object
+	 */
 	public Hand() {
 		
 		hand = new ArrayList<Card>();
 	}
 	
+	/**
+	 * Add card to hand
+	 * @param Card object
+	 */
 	public void addCard(Card card) {
 		
 		hand.add(card);
@@ -21,6 +42,12 @@ public class Hand {
 		return hand;
 	}
 	
+	/**
+	 * Remove cards from hand
+	 * @param index of cards to be removed
+	 * @param index of cards to be removed
+	 * @param index of cards to be removed 
+	 */
 	public void removeCardsFromHand(int index1, int index2, int index3) {
 		
 		if (canTurnInCards(index1, index2, index3) == true) {
@@ -33,6 +60,10 @@ public class Hand {
 		}
 	}
 	
+	/**
+	 * Check that whether cards must be turned in by user or not
+	 * @return whether cards must be turned in by user or not
+	 */
 	public boolean mustTurnInCards() {
 		
 		mustTurnInCards = false;
@@ -43,6 +74,13 @@ public class Hand {
 		return mustTurnInCards;
 	}
 	
+	/**
+	 * Check that whether player can turn in cards or not
+	 * @param index of card that can be turned in
+	 * @param index of card that can be turned in
+	 * @param index of card that can be turned in
+	 * @return whether player can turn in cards or not
+	 */
 	public boolean canTurnInCards(int index1, int index2, int index3) {
 		
 		mustTurnInCards = false;
