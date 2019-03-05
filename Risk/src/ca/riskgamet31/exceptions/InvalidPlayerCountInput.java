31595e21e39ff96cb1093560adfd63b7dd26a454
yash2696 <y_doshi@encs.concordia.ca>
2019-03-05 16:02:25 -0500

    added java doc

--- Risk/src/ca/riskgamet31/exceptions/InvalidPlayerCountInput.java
+++ Risk/src/ca/riskgamet31/exceptions/InvalidPlayerCountInput.java
@@ -1,5 +1,10 @@
 package ca.riskgamet31.exceptions;
 
+/**
+ * Exception if count for number of players is entered unvalid
+ * @author Yash Doshi
+ * @param Cause for the exception 
+ */
 public class InvalidPlayerCountInput extends Exception{
 	
 	public InvalidPlayerCountInput(String cause)
