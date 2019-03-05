42b372973abf8da61bcd75b424e5fb5bb3e3de8f
Chitra <chitradurga05@gmail.com>
2019-03-05 22:30:13 +0000

    TestGameMainDriver.java created online with Bitbucket

---
+++ Risk/src/ca/riskgamet31test/controllers/TestGameMainDriver.java
@@ -0,0 +1,42 @@
+
+package ca.riskgamet31test.controllers;
+import static org.junit.Assert.*;
+
+import javax.naming.InvalidNameException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ca.riskgamet31.controllers.GameMainDriver;
+import ca.riskgamet31.controllers.PlayerModel;
+import ca.riskgamet31.controllers.StartUpPhase;
+import ca.riskgamet31.maincomps.Player;
+
+
+public class TestGameMainDriver 
+{
+	static GameMainDriver G1;
+	static StartUpPhase s1;
+	static PlayerModel P1;
+	
+	@BeforeClass
+public  static void Testsetup()
+	{
+		G1=new GameMainDriver();
+		s1=new StartUpPhase();
+		P1=new PlayerModel();
+	}
+	
+	@Test
+public void TestcreatePlayer() throws NullPointerException, InvalidNameException
+	{
+		P1=G1.getPlayerList();
+		int a1=P1.getPlayerList().size();
+		G1.createPlayer();
+		int a2=P1.getPlayerList().size();
+	    assertNotEquals(a2, a1);
+		
+		
+	 }
+
+}
