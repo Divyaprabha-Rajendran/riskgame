a8a707be17284942ccdb2d564666e8b1125dc148
yash2696 <y_doshi@encs.concordia.ca>
2019-03-05 16:00:29 -0500

    added java doc

--- Risk/src/ca/riskgamet31/configuration/Constants.java
+++ Risk/src/ca/riskgamet31/configuration/Constants.java
@@ -1,14 +1,34 @@
 package ca.riskgamet31.configuration;
 
+/**
+ * Class for constants to define
+ * @author Yash Doshi
+ *
+ */
 public class Constants 
 {
+	/**
+	 * Armies if 3 players are playing
+	 */
 	public static final int ARMIES_3_PLAYERS = 35;
 	
+	/**
+	 * Armies if 4 players are playing
+	 */
 	public static final int ARMIES_4_PLAYERS = 30;
 	
+	/**
+	 * Armies if 5 players are playing
+	 */
 	public static final int ARMIES_5_PLAYERS = 25;
 	
+	/**
+	 * Armies if 6 players are playing
+	 */
 	public static final int ARMIES_6_PLAYERS = 20;
 	
+	/**
+	 * Armies if not valid player count is entered
+	 */
 	public static final int ARMIES_NOTVALID_PLAYERS = -1;
 }
