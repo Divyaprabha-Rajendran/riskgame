45620d5c6083886f38404999f14daca855552f40
Fareed Tayar <Fareed Tayar@192.168.2.22>
2019-03-14 19:48:43 -0400

    Two utility classes been added,fully tested along with error free jDocs

---
+++ Risk/src/ca/riskgamet31/utility/InputValidator.java
@@ -0,0 +1,76 @@
+package ca.riskgamet31.utility;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Utility class to validate input
+ * can be used for user input and/or game map file
+ * @author Fareed Tayar
+ * @version 2.0
+ * @since 2.0
+ */
+public class InputValidator
+  {
+	
+	/**
+	 * checks if input is composed of alphabet characters
+	 * @param input input to be validated
+	 * @return true if input is characters only
+	 */
+	public boolean validateCharacter(String input)
+	  {
+		input = (input == null) ? "" : input;
+		
+		if (input.length() > 0)
+		  {
+			Pattern stringPattern = Pattern.compile("^[a-zA-Z]*$");
+			Matcher matcher = stringPattern.matcher(input);
+			return matcher.matches();
+		  } else
+		  {
+			return false;
+		  }
+	  }
+	  
+	/**
+	 * checks if input is composed of numbers
+	 * @param input input to be validated
+	 * @return true if input is composed of numbers
+	 */
+	public boolean validateNumbers(String input)
+	  {
+		input = (input == null) ? "" : input;
+		
+		if (input.length() > 0)
+		  {
+			Pattern stringPattern = Pattern.compile("^[0-9]*$");
+			Matcher matcher = stringPattern.matcher(input);
+			return matcher.matches();
+		  } else
+		  {
+			return false;
+		  }
+	  }
+	  
+	/**
+	 * checks if input is composed of alpha-numeric characters
+	 * @param input input to be validated
+	 * @return true if input is composed of alpha-numeric
+	 */
+	public boolean validateAlphaNum(String input)
+	  {
+		
+		input = (input == null) ? "" : input;
+		if (input.length() > 0)
+		  {
+			Pattern stringPattern = Pattern.compile("^[a-zA-Z0-9]*$");
+			Matcher matcher = stringPattern.matcher(input);
+			return matcher.matches();
+		  } else
+		  {
+			return false;
+		  }
+	  }
+	  
+  }
---
+++ Risk/src/ca/riskgamet31/utility/UserInputRequester.java
@@ -0,0 +1,35 @@
+package ca.riskgamet31.utility;
+
+import java.util.Scanner;
+
+/**
+ * utility class for requesting an input from user
+ * @author Fareed Tayar
+ *@version 2.0
+ *@since 2.0
+ */
+public class UserInputRequester
+  {
+	
+	/**
+	 * utility method for requesting user input
+	 * @param inputMsg the message to show to user
+	 * @return user's input
+	 */
+	public String requestUserInput(String inputMsg)
+	  {
+		
+		Scanner sysIn = new Scanner(System.in);
+		String txt = "";
+		
+		do
+		  {
+			System.out.println(inputMsg);
+			if (sysIn.hasNext())
+			  txt = sysIn.next().trim().toUpperCase();
+		  } while (txt.length() == 0);
+		  
+		return txt;
+	  }
+	  
+  }
