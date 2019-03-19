039d86857596ac24eb504ad3eb641592d83b5309
ishpreet <>
2019-03-19 12:04:38 -0600

    junit for country

--- Risk/src/ca/riskgamet31test/maincomps/TestCountry.java
+++ Risk/src/ca/riskgamet31test/maincomps/TestCountry.java
@@ -1,10 +1,8 @@
-/**
- * 
- */
 package ca.riskgamet31test.maincomps;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -15,7 +13,7 @@ import org.junit.Test;
 import ca.riskgamet31.maincomps.Country;
 
 /**
- * test class to generate folders structure
+ * test class for testing country class
  * 
  * @author Ishpreet singh
  *
@@ -23,56 +21,72 @@ import ca.riskgamet31.maincomps.Country;
 public class TestCountry
   {
 	
-	/**
-	 * @throws java.lang.Exception
-	 */
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception
 	  {
 	  }
 	  
-	/**
-	 * @throws java.lang.Exception
-	 */
+	
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception
 	  {
 	  }
 	  
-	/**
-	 * @throws java.lang.Exception
-	 */
+	
 	@Before
 	public void setUp() throws Exception
 	  {
 	  }
 	  
-	/**
-	 * @throws java.lang.Exception
-	 */
+	
 	@After
 	public void tearDown() throws Exception
 	  {
 	  }
-	  
+	/**
+	 *To test if no country is passed dose null pointer exception is raised or not 
+	*@throws NullPointerException if null is passed
+	 */
+	  @Test(expected = NullPointerException.class)
+	  public void testCountry()
+	  {
+		  Country cn = new Country("null");  
+	  }
+	
+	/**
+	 * To test if armies are increased   
+	 */
 	@Test
 	public void testincreaseArmies()
 	  {
 		Country cn = new Country("india");
 		assertEquals(5, cn.increaseArmies(5));
 	  }
-	  
+	/**
+	 * To test if armies are reduced   
+	 */
+  
 	@Test
 	public void testreduceArmies()
 	  {
 		Country cn = new Country("india");
-		assertEquals(-99, cn.reduceArmies(5));
+		cn.increaseArmies(15);
+		assertEquals(10, cn.reduceArmies(5));
 	  }
-	  
+	  /**
+	   * To test if method equal work as desired
+	   */
 	@Test
 	public void testequals()
 	  {
-		Country cn = new Country("india");
-		assertFalse(cn.equals("pakistan"));
+		Country cn = new Country("INDIA");
+		assertFalse(cn.equals("PAKISTAN"));
+		Country cn1 = new Country("INDIA");
+		assertFalse(cn1.equals(""));
+		Country cn2 = new Country("INDIA");
+		assertTrue(cn2.equals("INDIA"));
+		
+		
 	  }
   }
