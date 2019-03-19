aa9c519320aeafd97c1e24033233fa9fba015fc3
Chitra_Gunasekaran <chitradurga05@gmail.com>
2019-03-19 15:56:30 -0400

    All tests Refactoring made in this class

--- Risk/src/ca/riskgamet31test/maincomps/AllTests.java
+++ Risk/src/ca/riskgamet31test/maincomps/AllTests.java
@@ -1,12 +1,22 @@
 package ca.riskgamet31test.maincomps;
 
 import org.junit.runner.RunWith;
+
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+/**
+ * 
+ * @author Chitra
+ * @version 1.0
+ * @since 1.0
+ *
+ */
 @RunWith(Suite.class)
+/**
+ * Run the all the test class at once.
+ */
 @SuiteClasses(
-  { TestContinent.class, TestCountry.class, TestGameMap.class, TestGraphNode.class })
+  { TestContinent.class,TestCard.class,TestDeckOfCards.class, TestCountry.class, TestGameMap.class,TestGraph.class, TestGraphNode.class })
 public class AllTests
   {
 	
