45ec11eac425d94c8a7d041776920c5d8d1d7d67
admin <admin@admin-PC>
2019-03-25 15:53:46 -0600

    junit for dice class

---
+++ Risk/src/ca/riskgamet31test/maincomps/TestDice.java
@@ -0,0 +1,124 @@
+package ca.riskgamet31test.maincomps;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ca.riskgamet31.maincomps.Dice;
+
+/**
+ * 
+ * @author Ishpreet singh
+ * @version 1.1
+ * Test class to check rolling functionality of dice
+ * In this we are get rolling dice 0.6 million times and getting probability of having 1 to 6 as output
+ * then comparing that with specified range
+ */
+public class TestDice {
+
+	static int[] rollResult;
+	static Dice dice;
+	static ArrayList<Integer> one;
+	static ArrayList<Integer> two;
+	static ArrayList<Integer> three;
+	static ArrayList<Integer> four;
+	static ArrayList<Integer> five;
+	static ArrayList<Integer> six;
+	
+	/**
+	 * getting dice and set up roll results.
+	 * @throws Exception throws exceptions
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	  {
+		rollResult = new int[600000];
+		dice = new Dice();
+		one = new ArrayList<>();
+		two = new ArrayList<>();
+		three = new ArrayList<>();
+		four = new ArrayList<>();
+		five = new ArrayList<>();
+		six = new ArrayList<>();
+	  }
+	  
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception
+	  {
+	  }
+	  
+	@Before
+	public void setUp() throws Exception
+	  {
+		
+		
+	  }
+	  	
+	@After
+	public void tearDown() throws Exception
+	  {
+	  }
+	
+	/**
+	 * testing the roll method by rolling it by 0.6 million times
+	 */
+	@Test
+	public void testRoll()
+	{
+		
+		rollResult = dice.roll(600000);
+		for(int i = 0; i < rollResult.length; i++)
+		{
+			if(rollResult[i] == 1)
+			{
+				one.add(1);
+			}
+			else if(rollResult[i] == 2)
+			{
+				two.add(1);
+			}
+			else if(rollResult[i] == 3)
+			{
+				three.add(1);
+			}
+			else if(rollResult[i] == 4)
+			{
+				four.add(1);
+			}
+			else if(rollResult[i] == 5)
+			{
+				five.add(1);
+			}
+			else if(rollResult[i] == 6)
+			{
+				six.add(1);
+			}
+		}
+		
+		float pOne = (float)one.size() / 600000;
+		float pTwo = (float)two.size() / 600000;
+		float pThree = (float)three.size() / 600000;
+		float pFour = (float)four.size() / 600000;
+		float pFive = (float)five.size() / 600000;
+		float pSix = (float)six.size() / 600000;
+		assertTrue(0.15 <= pOne && pOne <= 0.17);
+		assertTrue(0.15 <= pTwo && pTwo <= 0.17);
+		assertTrue(0.15 <= pThree && pThree <= 0.17);
+		assertTrue(0.15 <= pFour && pFour <= 0.17);
+		assertTrue(0.15 <= pFive && pFive <= 0.17);
+		assertTrue(0.15 <= pSix && pSix <= 0.17);
+//		System.out.println(pOne);
+//		System.out.println(pTwo);
+//		System.out.println(pThree);
+//		System.out.println(pFour);
+//		System.out.println(pFive);
+//		System.out.println(pSix);
+	}
+}
