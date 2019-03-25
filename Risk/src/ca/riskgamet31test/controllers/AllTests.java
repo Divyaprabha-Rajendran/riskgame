40e28d7b6b4201ffc0396834d4181fc4a9e17bf7
Chitra_Gunasekaran <chitradurga05@gmail.com>
2019-03-25 14:00:27 -0400

    test suite all test

--- Risk/src/ca/riskgamet31test/controllers/AllTests.java
+++ Risk/src/ca/riskgamet31test/controllers/AllTests.java
@@ -1,6 +1,7 @@
 package ca.riskgamet31test.controllers;
 
 import org.junit.runner.RunWith;
+
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 /**
@@ -15,8 +16,7 @@ import org.junit.runners.Suite.SuiteClasses;
 /**
  * Run the all the test class at once.
  */
-@SuiteClasses(
-  { TestGameMainDriver.class, TestPlayer.class, TestPlayerModel.class, TestStartupPhase.class })
+@SuiteClasses({ TestGameMainDriver.class, TestPlayerModel.class, TestStartupPhase.class,TestPlayer.class })
 public class AllTests
   {
 	
