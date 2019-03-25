2ae48376c54db7b21cdade0f3b22bbc6e63a6969
Chitra_Gunasekaran <chitradurga05@gmail.com>
2019-03-25 13:19:32 -0400

    Startup Phase of the game - Jtest function is created

--- Risk/src/ca/riskgamet31test/controllers/TestGameMainDriver.java
+++ Risk/src/ca/riskgamet31test/controllers/TestGameMainDriver.java
@@ -1,11 +1,12 @@
 
 package ca.riskgamet31test.controllers;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import javax.naming.InvalidNameException;
 
@@ -15,6 +16,13 @@ import org.junit.Test;
 import ca.riskgamet31.controllers.GameMainDriver;
 import ca.riskgamet31.controllers.PlayerModel;
 import ca.riskgamet31.controllers.StartUpPhase;
+import ca.riskgamet31.exceptions.InvalidContinentException;
+import ca.riskgamet31.exceptions.InvalidCountryException;
+import ca.riskgamet31.exceptions.InvalidGraphException;
+import ca.riskgamet31.exceptions.InvalidLinkException;
+import ca.riskgamet31.exceptions.InvalidPlayerException;
+import ca.riskgamet31.exceptions.InvalidPlayerNameException;
+import ca.riskgamet31.maincomps.Player;
 
 /**
  * Tests the GameMainDriver class for creating players
@@ -41,27 +49,36 @@ public class TestGameMainDriver
 	 * PlayerModel Class Reference 
 	 * 
 	 */
-	static PlayerModel P1;
+	static PlayerModel P1,pm;
 	/**
-	 * Object created before all the test method 
+	 *  Player Class Reference
 	 * 
 	 */
-	
+    static Player p1,p2;
+    /**
+     * Object created before all the test method
+     * @throws NullPointerException  when value is null
+     * @throws ca.riskgamet31.exceptions.InvalidNameException  when Name is invalid 
+     * @throws InvalidPlayerNameException when player name is duplicate
+     */
 	@BeforeClass
-	public static void Testsetup()
+	public static void Testsetup() throws NullPointerException, ca.riskgamet31.exceptions.InvalidNameException, InvalidPlayerNameException
 	  {
 		G1 = new GameMainDriver();
 		s1 = new StartUpPhase();
 		P1 = new PlayerModel();
+		pm=new PlayerModel();
+		p1=new  Player("p1",3);
+		p2=new Player("p2",4);
 		
 	  }
 	  
 	/**
 	 * 
 	 * Testing the Create Player method
-	 * @throws ca.riskgamet31.exceptions.InvalidNameException Gives Invalid Name Exception
-	 * @throws NullPointerException Gives null pointer exception
-	 * 
+	 * @throws ca.riskgamet31.exceptions.InvalidNameException when Name is invalid 
+	 * @throws NullPointerException when value is null
+	 *  
 	 */
 	@Test
 	public void TestcreatePlayer() throws NullPointerException,ca.riskgamet31.exceptions.InvalidNameException
@@ -76,5 +93,28 @@ public class TestGameMainDriver
 		assertNotEquals(a2, a1);
 		
 	  }
-	  
+	/**
+	 * 
+	 * Testing the Startup phase method
+	 *@throws InvalidGraphException when map is invalid graph
+	 *@throws ca.riskgamet31.exceptions.InvalidNameException when the Name given is invalid
+	 *@throws InvalidCountryException when the country is duplicate
+	 *@throws InvalidContinentException when the continent is duplicate
+	 *@throws InvalidLinkException when the link is disconnected
+	 * @throws Exception when there is exception is unexpectedly
+	 */
+	@Test
+	public void TestStartup() throws InvalidGraphException, ca.riskgamet31.exceptions.InvalidNameException, InvalidCountryException, InvalidContinentException, InvalidLinkException,Exception
+	{
+		File xmlFile = new File(System
+			    .getProperty("user.dir") + "\\Risk_MapData\\small_map.xml");
+		pm.setPlayerList(p1);
+		pm.setPlayerList(p2);
+	    s1.setPlayerCount(2);
+		G1.createGameMap(xmlFile.getPath());
+	    s1.distributeCountriesSequ(pm,G1.getGameMap());
+	    System.out.println(p1.getCountry());
+	    System.out.println(p2.getCountry());
+	    assertEquals("[INDIA:1:p1]",p1.getPlayerGraph().getGraphNodes().get(2).toString());
+       }
   }
