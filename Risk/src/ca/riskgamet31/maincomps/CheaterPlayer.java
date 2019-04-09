6591653c7c8c981f6e298990d03197e63b242075
Fareed Tayar <Fareed Tayar@192.168.2.22>
2019-04-09 12:25:38 -0400

    added missing jdoc for cheater player class, formated and clean

--- Risk/src/ca/riskgamet31/maincomps/CheaterPlayer.java
+++ Risk/src/ca/riskgamet31/maincomps/CheaterPlayer.java
@@ -1,13 +1,10 @@
 package ca.riskgamet31.maincomps;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import ca.riskgamet31.controllers.GameMainDriver;
 import ca.riskgamet31.controllers.MainDriver;
 import ca.riskgamet31.exceptions.InvalidNameException;
 import ca.riskgamet31.exceptions.InvalidPlayerNameException;
@@ -49,17 +46,14 @@ public class CheaterPlayer implements Player
 	 * @throws InvalidNameException       InvalidNameException
 	 * @throws InvalidPlayerNameException InvalidPlayerNameException
 	 */
-	
-	public CheaterPlayer(String playersName, int army) throws
-	    InvalidNameException, InvalidPlayerNameException
+	public CheaterPlayer(String playersName, int army)
+	    throws InvalidNameException, InvalidPlayerNameException
 	  {
 		this.playersName = playersName;
 		this.army = army;
 		playerCountryGraph = new Graph();
 		hand = new Hand();
 	  }
-	  
-	
 	  
 	/**
 	 * to get the name of the player
@@ -106,8 +100,6 @@ public class CheaterPlayer implements Player
 		
 	  }
 	  
-
-	  
 	/**
 	 * To increment reinforcement armies in player's graph
 	 * 
@@ -140,7 +132,6 @@ public class CheaterPlayer implements Player
 	  {
 		return this.army;
 	  }
-	  
 	  
 	/**
 	 * To add a new card to player's hand
@@ -187,8 +178,6 @@ public class CheaterPlayer implements Player
 		hand.removeCardsFromHand(cardIndexes[0], cardIndexes[1], cardIndexes[2]);
 	  }
 	  
-	
-	  
 	/**
 	 * get player graph
 	 * 
@@ -200,34 +189,45 @@ public class CheaterPlayer implements Player
 		return this.playerCountryGraph;
 	  }
 	  
+	/**
+	 * checks if Cheater player can fortifies.
+	 * 
+	 * @return an array list
+	 */
 	@Override
-	public ArrayList<GraphNode> canFortify(){
-	  
-	  ArrayList<GraphNode> returnedList = new ArrayList<>();
-	  
-	  ArrayList<GraphNode> destinationCountries = new ArrayList<>(this.getPlayerGraph().getGraphNodes()
-		  .stream().filter(i -> i.getNodeNeighbors().stream()
-			  .anyMatch(x -> !x.getNodeData().getCurrentOccupier().equals(this.getplayerName())) )
-		  .collect(Collectors.toList())); 
-	  
-	  
-	  	  return destinationCountries;
-	  
-	}
-	
-	@Override
-	public void fortification() {
-	  
-	  ArrayList<GraphNode> fortifyNodes = canFortify();
-	  
-	  for (GraphNode countryNode : fortifyNodes) {
-				
-		countryNode.getNodeData().increaseArmies(countryNode.getNodeData().getArmies());  
+	public ArrayList<GraphNode> canFortify()
+	  {
 		
+		ArrayList<GraphNode> returnedList = new ArrayList<>();
+		
+		ArrayList<GraphNode> destinationCountries = new ArrayList<>(this
+		    .getPlayerGraph().getGraphNodes().stream().filter(i -> i
+		        .getNodeNeighbors().stream().anyMatch(x -> !x.getNodeData()
+		            .getCurrentOccupier().equals(this.getplayerName())))
+		    .collect(Collectors.toList()));
+		
+		return destinationCountries;
 	  }
 	  
-	}
-	
+	/**
+	 * main fortification method
+	 */
+	@Override
+	public void fortification()
+	  {
+		
+		ArrayList<GraphNode> fortifyNodes = canFortify();
+		
+		for (GraphNode countryNode : fortifyNodes)
+		  {
+			
+			countryNode.getNodeData().increaseArmies(countryNode.getNodeData()
+			    .getArmies());
+			
+		  }
+		  
+	  }
+	  
 	/**
 	 * Fortification phase starting method according to user's entered armies,
 	 * fortification will be performed
@@ -236,30 +236,34 @@ public class CheaterPlayer implements Player
 	@Override
 	public void executeFortification(ArrayList<GraphNode> fortifyNodes)
 	  {
-		  
+		
 	  }
 	  
 	/**
-	 * Distribute the armies of every player among the countries the player
-	 * owns. The method executes till all the player's armies are distributed.
+	 * double the armies in player countries with bounded maximum of 100,000 to
+	 * avoid overflow.
 	 * 
 	 */
 	@Override
 	public void reinforcement()
 	  {
-		System.out.println("All Country armies will be doubled for " + this.getplayerName());
+		System.out.println("All Country armies will be doubled for " + this
+		    .getplayerName());
 		
-		for (GraphNode countryNode : this.getPlayerGraph().getGraphNodes()) {
-		  int reinforcementNumber = countryNode.getNodeData().getArmies()*2>=100000?100000:countryNode.getNodeData().getArmies()*2;
-		  countryNode.getNodeData().setArmies(reinforcementNumber);
+		for (GraphNode countryNode : this.getPlayerGraph().getGraphNodes())
+		  {
+			int reinforcementNumber = countryNode.getNodeData()
+			    .getArmies() * 2 >= 100000 ? 100000 : countryNode.getNodeData()
+			        .getArmies() * 2;
+			countryNode.getNodeData().setArmies(reinforcementNumber);
+			
+		  }
 		  
-		}
-		
-		System.out.println("All Countries armies are doubled for " + this.getplayerName());
+		System.out.println("All Countries armies are doubled for " + this
+		    .getplayerName());
 		
 		this.decrementArmies(this.getPlayerArmies());
-			
-		  
+		
 	  }
 	  
 	/**
@@ -288,74 +292,79 @@ public class CheaterPlayer implements Player
 		
 		this.getPlayerGraph().viewGraph();
 		
-		ArrayList<GraphNode> destinationCountries = new ArrayList<>(this.getPlayerGraph().getGraphNodes()
-			  .stream().filter(x -> x.getNodeData().getArmies() > 1).filter(i -> i.getNodeNeighbors().stream()
-				  .anyMatch(x -> !x.getNodeData().getCurrentOccupier().equals(this.getplayerName())) )
-			  .collect(Collectors.toList())); 
-		  
+		ArrayList<GraphNode> destinationCountries = new ArrayList<>(this
+		    .getPlayerGraph().getGraphNodes().stream().filter(x -> x
+		        .getNodeData().getArmies() > 1).filter(i -> i.getNodeNeighbors()
+		            .stream().anyMatch(x -> !x.getNodeData()
+		                .getCurrentOccupier().equals(this.getplayerName())))
+		    .collect(Collectors.toList()));
 		
-       for (GraphNode countryNode : destinationCountries) {
-		  
-    	 
-    	 ArrayList<GraphNode> otherPlayersNeighbors = new ArrayList<>(countryNode.getNodeNeighbors().stream()
-    		 .filter(x -> !x.getNodeData().getCurrentOccupier().equals(this.getplayerName())
-       		  ).collect(Collectors.toList()));
-    	 
-    	  for (GraphNode neighbor : otherPlayersNeighbors) {
-    		
-    		
-    		Player defendingPlayer = null;
-    		
-    		for (Player defender : driver.getPlayerList().getPlayerList())
-  			  {
-  				
-  				if (defender.getplayerName().equals(neighbor
-  				    .getNodeData().getCurrentOccupier()))
-  				  {
-  					defendingPlayer = defender;
-  					
-  				  }	
-  			  } 
-    		  
-    		  if (countryNode.getNodeData().getArmies()>1)
-    			{  
-    		  neighbor.getNodeData().setCurrentOccupier(this.getplayerName());
-	    		  neighbor.getNodeData().setArmies(1);
-	    		  countryNode.getNodeData().setArmies(countryNode.getNodeData().getArmies()-1);
-	    		  
-	    		  if (won == false) won = true;
-	    		  
-	    		  this.getPlayerGraph().addNode(neighbor);
-	    		  
-	    		  defendingPlayer.removeCountry(neighbor);
+		for (GraphNode countryNode : destinationCountries)
+		  {
+			
+			ArrayList<GraphNode> otherPlayersNeighbors = new ArrayList<>(countryNode
+			    .getNodeNeighbors().stream().filter(x -> !x.getNodeData()
+			        .getCurrentOccupier().equals(this.getplayerName()))
+			    .collect(Collectors.toList()));
+			
+			for (GraphNode neighbor : otherPlayersNeighbors)
+			  {
+				
+				Player defendingPlayer = null;
+				
+				for (Player defender : driver.getPlayerList().getPlayerList())
+				  {
+					
+					if (defender.getplayerName().equals(neighbor.getNodeData()
+					    .getCurrentOccupier()))
+					  {
+						defendingPlayer = defender;
+						
+					  }
+				  }
+				  
+				if (countryNode.getNodeData().getArmies() > 1)
+				  {
+					neighbor.getNodeData().setCurrentOccupier(this
+					    .getplayerName());
+					neighbor.getNodeData().setArmies(1);
+					countryNode.getNodeData().setArmies(countryNode
+					    .getNodeData().getArmies() - 1);
+					
+					if (won == false)
+					  won = true;
+					
+					this.getPlayerGraph().addNode(neighbor);
+					
+					defendingPlayer.removeCountry(neighbor);
 					
 					if (defendingPlayer.getPlayerCountriesGNodes().size() == 0)
-		  			  {
-		  				
-		  				System.out.println(this
-		  				    .getplayerName() + " will receive - " + defendingPlayer.getHand()
-		  				        .getCardsFromHand()
-		  				        .size() + " - cards from player " + defendingPlayer
-		  				            .getplayerName());
-		  				
-		  				for (Card card : defendingPlayer.getHand().getCardsFromHand())
-		  				  {
-		  					this.addNewCard(card);
-		  				  }
-		  				  
-		  				driver.getPlayerList().getPlayerList().remove(defendingPlayer);
-		  				System.out.println("Player " + defendingPlayer
-		  				    .getplayerName() + " is removed from game.");
-					
-		  			  }
-    		  
-    	  }
-    		  
-    		  
-    		  
-    	  }
-       }
-    	 return won;
+					  {
+						
+						System.out.println(this
+						    .getplayerName() + " will receive - " + defendingPlayer
+						        .getHand().getCardsFromHand()
+						        .size() + " - cards from player " + defendingPlayer
+						            .getplayerName());
+						
+						for (Card card : defendingPlayer.getHand()
+						    .getCardsFromHand())
+						  {
+							this.addNewCard(card);
+						  }
+						  
+						driver.getPlayerList().getPlayerList()
+						    .remove(defendingPlayer);
+						System.out.println("Player " + defendingPlayer
+						    .getplayerName() + " is removed from game.");
+						
+					  }
+					  
+				  }
+				  
+			  }
+		  }
+		return won;
 	  }
 	  
 	/**
@@ -368,62 +377,72 @@ public class CheaterPlayer implements Player
 	 * @return true if attacker occupied the attacked country.
 	 */
 	@Override
-	public boolean attackRound(MainDriver driver,
-	    GraphNode attackerCountryNode, GraphNode defenderCountryNode,
-	    boolean allOut)
+	public boolean attackRound(MainDriver driver, GraphNode attackerCountryNode,
+	    GraphNode defenderCountryNode, boolean allOut)
 	  {
-			  
+		
 		return true;
 	  }
-	
-	  /**
-		 * to execute exchange of cards
-		 * @param gameMainDriver current gameMainDriver of the game
-		 * @param request selected cards by the player
-		 * @return true if exchange been successful
-		 */
+	  
+	/**
+	 * to execute exchange of cards
+	 * 
+	 * @param gameMainDriver current gameMainDriver of the game
+	 * @param request        selected cards by the player
+	 * @return true if exchange been successful
+	 */
+	public boolean executeTurnInCard(MainDriver gameMainDriver, String request)
+	  {
+		Map<String, List<Card>> handsCards = this.getHand().getCardsFromHand()
+		    .stream().collect(Collectors
+		        .groupingBy(Card::getCardType, Collectors.toList()));
 		
-	public boolean executeTurnInCard(MainDriver gameMainDriver,
-		    String request)
-		  {      
-			Map<String,List<Card>> handsCards = this.getHand().getCardsFromHand().stream()
-				.collect(Collectors.groupingBy(Card::getCardType,Collectors.toList()));
-			  
-			if (handsCards.size() == 3) {
-			 
-			  for (List<Card> listOfCards : handsCards.values()) {
-				  
+		if (handsCards.size() == 3)
+		  {
+			
+			for (List<Card> listOfCards : handsCards.values())
+			  {
+				
 				this.getHand().getCardsFromHand().remove(listOfCards.get(0));
-				}
+			  }
 			  
-			}else {
+		  } else
+		  {
+			
+			boolean continueDelete = true;
+			
+			for (List<Card> listOfCardsPerType : handsCards.values())
+			  {
+				
+				if (listOfCardsPerType.size() >= 3 && continueDelete)
+				  {
+					
+					this.getHand().getCardsFromHand().remove(listOfCardsPerType
+					    .get(0));
+					this.getHand().getCardsFromHand().remove(listOfCardsPerType
+					    .get(1));
+					this.getHand().getCardsFromHand().remove(listOfCardsPerType
+					    .get(2));
+					continueDelete = false;
+				  }
+			  }
 			  
-			  boolean continueDelete = true;
-			  
-			  for (List<Card> listOfCardsPerType : handsCards.values()) {
-				  
-				if (listOfCardsPerType.size() >= 3 && continueDelete) {
-				  
-				  this.getHand().getCardsFromHand().remove(listOfCardsPerType.get(0));
-				  this.getHand().getCardsFromHand().remove(listOfCardsPerType.get(1));
-				  this.getHand().getCardsFromHand().remove(listOfCardsPerType.get(2));
-				  continueDelete = false;
-				}
-				}
-			  
-			}
-					this.setArmies(this
-					    .getPlayerArmies() + (Constants.turnInCards++ * 5));
-					System.out.println("Now " + this
-					    .getplayerName() + ": is eligible for " + this
-					        .getPlayerArmies() + " armies");
-					System.out.println("Current player's hand has " + hand
-					    .getCardsFromHand()); 
-			  
-			return true;
 		  }
-
-	
+		this.setArmies(this.getPlayerArmies() + (Constants.turnInCards++ * 5));
+		System.out.println("Now " + this
+		    .getplayerName() + ": is eligible for " + this
+		        .getPlayerArmies() + " armies");
+		System.out.println("Current player's hand has " + hand
+		    .getCardsFromHand());
+		
+		return true;
+	  }
+	  
+	/**
+	 * check if cheater player may attack or not
+	 * 
+	 * @return attacking and attacked countries
+	 */
 	@Override
 	public ArrayList<GraphNode> canAttack()
 	  {
@@ -458,27 +477,31 @@ public class CheaterPlayer implements Player
 		  
 		return adNodes;
 	  }
-
-		/**
-		 * To get the dice input after rolling it
-		 * 
-		 * @param countryObj country of attacker of defender
-		 * @param i          identifier that whether dice is rolling for attacker or
-		 *                   defender
-		 * @return output of rolled dice input
-		 */
-		@Override
-		public int getDiceInput(Country countryObj, String i)
-		  {
-					return 0;
-		  }
-
+	  
+	/**
+	 * To get the dice input after rolling it
+	 * 
+	 * @param countryObj country of attacker of defender
+	 * @param i          identifier that whether dice is rolling for attacker or
+	 *                   defender
+	 * @return output of rolled dice input
+	 */
+	@Override
+	public int getDiceInput(Country countryObj, String i)
+	  {
+		return 0;
+	  }
+	  
+	/**
+	 * utility method for reinforcement main method
+	 * 
+	 * @return a Dummy country as cheater will double his armies
+	 */
+	@Override
+	public GraphNode canReinforce()
+	  {
 		
-	  @Override
-	  public GraphNode canReinforce()
-		{
-		  
-		  return new GraphNode(new Country("DUMMY"));
-		}
+		return new GraphNode(new Country("DUMMY"));
+	  }
 	  
   }
