63033f2f22f0d9ba3968b35ebf2c881912cd9024
Fareed Tayar <Fareed Tayar@192.168.2.22>
2019-04-09 19:26:20 -0400

    project document for build three

--- Risk/src/ca/riskgamet31test/utility/LoadGameTest.java
+++ Risk/src/ca/riskgamet31test/utility/LoadGameTest.java
@@ -1,11 +1,11 @@
 package ca.riskgamet31test.utility;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 //import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.HashMap;
@@ -26,25 +26,31 @@ import ca.riskgamet31.utility.Backup;
 import ca.riskgamet31.views.PlayersWorldDominationView;
 
 /**
- * To test the loading of game from the serialized object. 
+ * To test the loading of game from the serialized object.
+ * 
  * @author Divyaprabha Rajendran
  * @version 1.0
  */
-public class LoadGameTest {
-
+public class LoadGameTest
+  {
+	
 	/**
 	 * GameMainDriver for Loading the game.
 	 */
 	static GameMainDriver game;
+	
 	/**
 	 * Test the creation of GameMainDriver object to be serialized.
+	 * 
 	 * @throws InvalidLinkException Invalid link in graph.
-	 * @throws Exception Exception.
+	 * @throws Exception            Exception.
 	 */
 	@Test
-	public void testLoadGame() throws InvalidLinkException, Exception {
-		game=new GameMainDriver();
-		String xmlPath = System.getProperty("user.dir")+ "\\Risk_MapData\\small_map.xml";
+	public void testLoadGame() throws InvalidLinkException, Exception
+	  {
+		game = new GameMainDriver();
+		String xmlPath = System
+		    .getProperty("user.dir") + "\\Risk_MapData\\small_map.xml";
 		CreateMap cmap = new CreateMap(xmlPath);
 		HashMap<String, Continent> continentsList = cmap.generateGraph();
 		Graph gameMapGraph = new Graph(cmap.getAllCountryNodes());
@@ -52,41 +58,49 @@ public class LoadGameTest {
 		game.setGameMap(risk);
 		game.setTurnInCardsCount(10);
 		PlayerModel pm = new PlayerModel();
-		pm.setPlayerList(new HumanPlayer("human",4));
-		pm.setPlayerList(new AggressivePlayer("dragon",10));
-	    game.setPlayers(pm);
-	    game.setTurnID(2);
-	    DeckOfCards deck = new DeckOfCards(game.getGameMap().getGameMapGraph().getGraphNodes());
-	    game.setDeck(deck);
-	    game.setPlayerWorldDominationView(new PlayersWorldDominationView());
-	    assertNotNull(game);
-	}
-
+		pm.setPlayerList(new HumanPlayer("human", 4));
+		pm.setPlayerList(new AggressivePlayer("dragon", 10));
+		game.setPlayers(pm);
+		game.setTurnID(2);
+		DeckOfCards deck = new DeckOfCards(game.getGameMap().getGameMapGraph()
+		    .getGraphNodes());
+		game.setDeck(deck);
+		game.setPlayerWorldDominationView(new PlayersWorldDominationView());
+		assertNotNull(game);
+	  }
+	  
 	/**
 	 * To test the creation of serialized object in a file.
-	 * @throws IOException IOException 
+	 * 
+	 * @throws IOException            IOException
 	 * @throws ClassNotFoundException ClassNotFoundException
 	 */
 	@Test
-	public void testResumeGame() throws IOException, ClassNotFoundException {
+	public void testResumeGame() throws IOException, ClassNotFoundException
+	  {
 		
 		String xmlFilePath = System
-			    .getProperty("user.dir") +"\\Risk_MapData\\game_obj.ser";
-    	    	
-    	FileInputStream file = new FileInputStream(xmlFilePath); 
-        ObjectInputStream in = new ObjectInputStream(file); 
-           
-        Backup currBkp = (Backup)in.readObject(); 
-          
-        in.close(); 
-        file.close(); 
-        
-        assertEquals(currBkp.getGameMap().getGameMapFile(),game.getGameMap().getGameMapFile());
-        assertEquals(currBkp.getGameMap().getContinentsList().size(), game.getGameMap().getContinentsList().size());
-        assertEquals(currBkp.getGameMap().getGameMapGraph().getGraphNodes().size(), game.getGameMap().getGameMapGraph().getGraphNodes().size());
-        assertEquals(currBkp.getPlayers().getPlayerList().size(),game.getPlayerList().getPlayerList().size());
-        assertEquals(currBkp.getTurnId(), game.getTurnID());
-        assertEquals(currBkp.getTurnInCardsCount(), game.getTurnInCardsCount());
-	}
-
-}
+		    .getProperty("user.dir") + "\\Risk_MapData\\game_obj.ser";
+		
+		FileInputStream file = new FileInputStream(xmlFilePath);
+		ObjectInputStream in = new ObjectInputStream(file);
+		
+		Backup currBkp = (Backup) in.readObject();
+		
+		in.close();
+		file.close();
+		
+		assertEquals(currBkp.getGameMap().getGameMapFile(), game.getGameMap()
+		    .getGameMapFile());
+		assertEquals(currBkp.getGameMap().getContinentsList().size(), game
+		    .getGameMap().getContinentsList().size());
+		assertEquals(currBkp.getGameMap().getGameMapGraph().getGraphNodes()
+		    .size(), game.getGameMap().getGameMapGraph().getGraphNodes()
+		        .size());
+		assertEquals(currBkp.getPlayers().getPlayerList().size(), game
+		    .getPlayerList().getPlayerList().size());
+		assertEquals(currBkp.getTurnId(), game.getTurnID());
+		assertEquals(currBkp.getTurnInCardsCount(), game.getTurnInCardsCount());
+	  }
+	  
+  }
